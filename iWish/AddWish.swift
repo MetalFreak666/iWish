23604d66e08a66567f0d4762140b67a007ebaf83
Dariusz <egonoras@hotmail.com>
2019-11-07 11:35:17 +0100

    Added wishes screen

---
+++ iWish/AddWish.swift
@@ -0,0 +1,9 @@
+//
+//  AddWish.swift
+//  iWish
+//
+//  Created by Dariusz Orasinski on 07/11/2019.
+//  Copyright © 2019 SDU. All rights reserved.
+//
+
+import Foundation
---
+++ iWish/MyWishesController.swift
@@ -0,0 +1,20 @@
+//
+//  MyWishesController.swift
+//  iWish
+//
+//  Created by Dariusz Orasinski on 07/11/2019.
+//  Copyright © 2019 SDU. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyWishesController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
