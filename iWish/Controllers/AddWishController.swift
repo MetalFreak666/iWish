b7bf6f8c4305af1ca8d9c9821d9786e89ab860c5
Dariusz <egonoras@hotmail.com>
2019-11-08 16:39:57 +0100

    fixed issues in AddWishController

--- iWish/Controllers/AddWishController.swift
+++ iWish/Controllers/AddWishController.swift
@@ -15,16 +15,29 @@ class AddWishController: UIViewController, UIImagePickerControllerDelegate, UINa
     //TextFields title and description
     @IBOutlet weak var wishTitle: UITextField!
     @IBOutlet weak var wishDescription: UITextField!
-    @IBOutlet weak var test: UITextField!
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var test: UILabel!
+    @IBOutlet weak var test: UITextField!
+    @IBOutlet weak var wishPrice: UITextField!
     
-        wish = Wish(title: wishTitle.text, description: wishDescription.text)
-    
+    @IBAction func addWish(_ sender: Any) {
+        
+        if let input = wishTitle.text, input.isEmpty {
+            test.text = "Title cannot be empty"
+        } else if let input = wishPrice.text, input.isEmpty {
+            test.text = "Price cannot be empty"
+        //Proceed from her
+        } else {
+            wish = Wish(title: wishTitle.text, description: wishDescription.text, price: wishPrice.text)
+            test.text = nil
+            //Test
+            //print(wish!)
+        }
+        
     }
-
-
-
+    
+    
+  
+    
     @IBAction func chooseImage(_ sender: Any) {
         
         let imagePickerController = UIImagePickerController()
@@ -67,58 +80,7 @@ class AddWishController: UIViewController, UIImagePickerControllerDelegate, UINa
         picker.dismiss(animated: true, completion: nil)
     }
 
-        }
-    }
     
-    @IBAction func chooseImage(_ sender: Any) {
-        
-        let imagePickerController = UIImagePickerController()
-        imagePickerController.delegate = self
-        
-        
-        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
-        
-        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action:UIAlertAction) in
-            
-            if UIImagePickerController.isSourceTypeAvailable(.camera) {
-                imagePickerController.sourceType = .camera
-                self.present(imagePickerController, animated: true, completion: nil)
-            } else {
-                print("Device has no camera..")
-            }
-            imagePickerController.sourceType = .camera
-            self.present(imagePickerController, animated: true, completion: nil)
-            
-        }))
-        
-        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {(action:UIAlertAction) in imagePickerController.sourceType = .photoLibrary
-            self.present(imagePickerController, animated: true, completion: nil)
-        }))
-        
-        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
-        
-        self.present(actionSheet, animated: true, completion: nil)
-    }
-    
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-        picker.dismiss(animated: true)
-        
-        let image = info[.originalImage] as! UIImage
-        imageView.image = image
-    }
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true, completion: nil)
-    }
-    
-    //Used to taking photo
-    @IBAction func onActionTakePhoto(_ sender: Any) {
-        //imagePicker = UIImagePickerController()
-        //imagePicker.delegate = self
-        //imagePicker.sourceType = .camera
-        //present(imagePicker, animated: true, completion: nil)
-    }
        
     override func viewDidLoad() {
         super.viewDidLoad()
