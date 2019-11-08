bf422a26d560dc14e8bae3dbab88c31420396f27
Jebisan H. Nadarajah <Jebisan.h.n@hotmail.com>
2019-11-08 16:08:37 +0100

    Added camera feature

--- iWish/Controllers/AddWishController.swift
+++ iWish/Controllers/AddWishController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AddWishController: UIViewController {
+class AddWishController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     private var wish: Wish?
         
@@ -16,10 +16,55 @@ class AddWishController: UIViewController {
     @IBOutlet weak var wishTitle: UITextField!
     @IBOutlet weak var wishDescription: UITextField!
     @IBOutlet weak var test: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
     
     @IBAction func onActionAddWish(_ sender: UIButton) {
         wish = Wish(title: wishTitle.text, description: wishDescription.text)
         
+    }
+    
+
+    
+    @IBAction func chooseImage(_ sender: Any) {
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action:UIAlertAction) in
+            
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+                } else {
+                print("Device has no camera..")
+                }
+            imagePickerController.sourceType = .camera
+            self.present(imagePickerController, animated: true, completion: nil)
+            
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {(action:UIAlertAction) in imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+
+    self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        picker.dismiss(animated: true)
+        
+        let image = info[.originalImage] as! UIImage
+        imageView.image = image
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
     
     
