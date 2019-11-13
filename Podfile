aaf2042ef41fa98e178e1b12f4a1613946efae5c
Jebisan H. Nadarajah <Jebisan.h.n@hotmail.com>
2019-11-13 17:22:18 +0100

    Added facebook login

--- Podfile
+++ Podfile
@@ -1,12 +1,18 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '10.0'
 
 target 'iWish' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for iWish
-  pod 'Firebase/Auth'
   pod 'Firebase/Database'
+  
+  pod 'FBSDKCoreKit'
+  pod 'FBSDKLoginKit'
+  pod 'FBSDKShareKit'
+  pod 'FacebookLogin'
+
+
 
 end
--- iWish/Controllers/LoginViewController.swift
+++ iWish/Controllers/LoginViewController.swift
@@ -7,14 +7,72 @@
 //
 
 import UIKit
+import FacebookLogin
+import FacebookCore
+
 
 class LoginViewController: UIViewController {
+      
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Welcome to the Login screen!");
     }
     
     
-}
+    func redirectToTabBar () {
+        let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "TabBarController") as UIViewController
+        self.present(viewController, animated: false, completion: nil)
+    }
+    
 
+    @IBAction func loginWithFacebook(_ sender: UIButton) {
+        self.loginWithReadPermissions()
+        
+    }
+    
+    func loginManagerDidComplete(_ result: LoginResult) {
+        let alertController: UIAlertController
+        switch result {
+        case .cancelled:
+            alertController = UIAlertController(
+                title: "Login Cancelled",
+                message: "User cancelled login.",
+                preferredStyle: .alert
+            )
+            
+        case .failed(let error):
+            alertController = UIAlertController(
+                title: "Login Fail",
+                message: "Login failed with error \(error)",
+                preferredStyle: .alert
+            )
+            
+        case .success(let grantedPermissions, _, _):
+            redirectToTabBar()
+            
+        }
+    }
+    
+    @IBAction private func loginWithReadPermissions() {
+        let loginManager = LoginManager()
+        loginManager.logIn(
+            permissions: [.publicProfile, .userFriends],
+            viewController: self
+        ) { result in
+            self.loginManagerDidComplete(result)
+        }
+    }
+    
+    @IBAction private func logOut() {
+        let loginManager = LoginManager()
+        loginManager.logOut()
+        
+        let alertController = UIAlertController(
+            title: "Logout",
+            message: "Logged out.",
+            preferredStyle: .alert
+        )
+        present(alertController, animated: true, completion: nil)
+    }
+    
+}
