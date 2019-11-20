865f28778a917a4bcd1b27d876f1ae28cdc9c61c
Dariusz Orasinski <egonoras@hotmail.com>
2019-11-20 12:17:54 -0800

    second commit

--- iWish/Controllers/MyWishesViewController.swift
+++ iWish/Controllers/MyWishesViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class MyWishesViewController: UIViewController {
     
+    private var wishManager: WishManager!
+    private var wishes: [Wish]!
+    
+    @IBOutlet weak var wishTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //init
+        wishManager = WishManager()
+        wishManager.createWish()
+        wishes = wishManager.myWishes
+        
+        wishTableView.dataSource = self
+        wishTableView.delegate = self
+        
         print("Welcome to the My Wishes screen!");
     }
-    
-    
+   
 }
+
+extension MyWishesViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return wishes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let wish = wishes[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WishTableViewCell") as! WishTableViewCell
+        
+        cell.wishTitleLabel.text = wish.title
+        cell.wishDescriptionLabel.text = wish.wishDescription
+        cell.wishPriceLabel.text = String(wish.price)
+        
+        return cell
+    }
+}
+
 
---
+++ iWish/Controllers/WishTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  WishTableViewCell.swift
+//  iWish
+//
+//  Created by Dariusz Orasin on 20/11/2019.
+//  Copyright © 2019 SDU. All rights reserved.
+//
+
+import UIKit
+
+class WishTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var wishTitleLabel: UILabel!
+    @IBOutlet weak var wishDescriptionLabel: UILabel!
+    @IBOutlet weak var wishPriceLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
